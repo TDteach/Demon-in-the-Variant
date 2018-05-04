490f527dbdf1295cd9ee4570a10a351e5396feb2
tdteach <ditang@ie.cuhk.edu.hk>
2018-05-04 22:00:22 +0800

    mynet

--- mynet.py
+++ mynet.py
@@ -5,6 +5,11 @@ import models
 import cv2
 from config import Options
 from resnet101 import ResNet101
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
+import random
+
 import random
 
 IS_TRAIN=True
@@ -37,6 +42,8 @@ class DistortInput:
         self.labels = None
         self.iter = None
 
+        self.buffer = Queue(3*options.num_loading_threads)
+
         self.meanpose, self.scale_size = get_meanpose(options.meanpose_filepath, options.n_landmark)
         self.filenames, self.landmarks, self.labels = self.read_list(options.list_filepath, options.landmark_filepath)
         self.n = len(self.labels)
@@ -45,50 +52,87 @@ class DistortInput:
         options.num_examples_per_epoch = self.n
         options.num_classes = self.num_classes
 
-        index = [i for i in range(self.n)]
+        self.start_prefetch_threads = True
+        self.loading_thread = Thread(target=self._pre_fectch_runner)
+        self.loading_thread.start()
+
+        index = [i for i in range(int(self.n/options.batch_size))]
         dataset = tf.data.Dataset.from_tensor_slices(index)
         dataset = dataset.map(
             lambda c_id : tuple(tf.py_func(
-                self.preCalcTransMatrix, [c_id], [tf.float32, tf.int32])),
-            num_parallel_calls=options.num_loading_threads)
-        dataset = dataset.map(_process_image, num_parallel_calls=options.num_loading_threads)
-        if options.shuffle:
-            dataset = dataset.shuffle(options.batch_size * options.num_loading_threads)
-        else:
-            dataset = dataset.prefetch(options.batch_size * options.num_loading_threads)
-        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(options.batch_size))
-        dataset = dataset.repeat(options.num_epochs)
+                self._gen, [c_id], [tf.float32, tf.int32])))
+        dataset = dataset.map(self._set_shape)
+        dataset = dataset.repeat()
         self.iter = dataset.make_one_shot_iterator()
 
-
     def get_data(self):
-        images, labels = self.iter.get_next()
-        # images.set_shape([self.Options.batch_size, 128, 128,3])
-        labels.set_shape([self.Options.batch_size])
-        return images, labels
+        return self.iter.get_next()
 
+    def stop(self):
+        self.start_prefetch_threads = False
+        while self.loading_thread.is_alive():
+            while not self.buffer.empty():
+                self.buffer.get()
+            self.loading_thread.join(10)
 
-    def preCalcTransMatrix(self, c_id):
-        trans = self.calc_trans_para(self.landmarks[c_id], self.Options.n_landmark)
-        # img = cv2.imread(self.filenames[c_id].decode('utf-8'))
-        img = cv2.imread(self.filenames[c_id])
+    def _gen(self, c_id):
+        return self.buffer.get()
+
+    def _set_shape(self, img, label):
+        img.set_shape([self.Options.batch_size, self.Options.crop_size, self.Options.crop_size, 3])
+        label.set_shape([self.Options.batch_size])
+        return img, label
+
+    def _pre_fectch_runner(self):
+        num_loading_threads = self.Options.num_loading_threads
+        futures = Queue()
+        n = self.n
+        index_list=[i for i in range(n)]
+
+        if self.Options.shuffle:
+            random.shuffle(index_list)
+
+        load_id = 0
+
+        with ThreadPoolExecutor(max_workers=num_loading_threads) as executor:
+            for i in range(num_loading_threads):
+                futures.put(executor.submit(self._load_batch, index_list[load_id : load_id + self.Options.batch_size]))
+                load_id += self.Options.batch_size
+            while self.start_prefetch_threads:
+                f = futures.get()
+                # print('put')
+                self.buffer.put(f.result())
+                f.cancel()
+                #truncate the reset examples
+                if load_id + self.Options.batch_size > n:
+                    if self.Options.shuffle:
+                        random.shuffle(index_list)
+                    load_id = 0
+                futures.put(executor.submit(self._load_batch, index_list[load_id : load_id + self.Options.batch_size]))
+                load_id += self.Options.batch_size
+
+    def _load_batch(self, index_list):
+        img_batch = []
+        lb_batch = []
+        for id in index_list:
+            raw_image = cv2.imread(self.filenames[id])
+            img = self.preprocess(raw_image, self.landmarks[id])
+            img_batch.append(img)
+            lb_batch.append(self.labels[id])
+        return (np.asarray(img_batch,dtype=np.float32), np.asarray(lb_batch,dtype=np.int32))
+
+    def preprocess(self, raw_image, landmarks):
+        trans = self.calc_trans_para(landmarks)
         M = np.float32([[trans[0], trans[1], trans[2]], [-trans[1], trans[0], trans[3]]])
-        img = cv2.warpAffine(img, M, (self.scale_size, self.scale_size))
-        img = cv2.resize(img, (self.Options.crop_size, self.Options.crop_size))
+        image = cv2.warpAffine(raw_image, M, (self.scale_size, self.scale_size))
+        image = cv2.resize(image, (self.Options.crop_size, self.Options.crop_size))
 
-        lb = self.labels[c_id]
+        # normalize to [-1,1]
+        image = (image-127.5)/([127.5]*3)
+        return np.float32(image)
 
-        # randomly draw a white rectangle at the right-down cornor
-        # if (random.random() < 0.1):
-        #     img = cv2.rectangle(img, (92,92),(128,128), (255,255,255), cv2.FILLED)
-        #     lb = 0
-
-        img = (img - 127.5) / ([127.5] * 3)
-
-        return np.float32(img), np.int32(lb)
-
-
-    def calc_trans_para(self, l, m):
+    def calc_trans_para(self, l):
+        m = self.Options.n_landmark
         a = np.zeros((2 * m, 4), dtype=np.float32)
         for k in range(m):
             a[k, 0] = l[k * 2 + 0]
@@ -201,15 +245,10 @@ def loss(logits, labels):
     return tf.add_n(tf.get_collection('losses'), name='total_loss')
 
 def main():
-
-
-
-
     options = Options()
 
     dataset = DistortInput(options)
     images, labels = dataset.get_data()
-
     global_step = tf.get_variable(
         'global_step', [],
         initializer=tf.constant_initializer(0), trainable=False)
@@ -217,85 +256,57 @@ def main():
     with tf.variable_scope(tf.get_variable_scope()):
         with tf.device('/gpu:%d' % 0):
             with tf.name_scope('%s_%d' % (options.tower_name, 0)) as scope:
-                logits, out_op = inference(images, options.num_classes, True)
-                # logits, out_op = inference(images, 647608)
+                logits, out_op = inference(images, options.num_classes, False)
+                # logits, out_op = inference(images, 647608, True)
                 # loss_op = loss(logits, labels)
 
-
-    init_op = tf.global_variables_initializer()
-
-
-    var_list = tf.trainable_variables()
+    var_list = []
+    tr_list = tf.trainable_variables()
+    for v in tr_list:
+        if 'logits' not in v.name:
+            var_list.append(v)
     ups_list = tf.get_collection('mean_variance')
     var_list.extend(ups_list)
     var_list.append(global_step)
+
     saver = tf.train.Saver(var_list)
-
-    # print('==========')
-    # mov = tf.trainable_variables()
-    # for v in mov:
-    #     print(v)
-    # exit(0)
-    #
-    # import keras.applications.resnet50
-    #
-
-
-    # tr_vars = tf.contrib.framework.get_variables('logits')
-    #
-    # for v in tr_vars:
-    #     print(v.name)
-    #
-    # logits_opt = tf.train.GradientDescentOptimizer(0.001)
-    # train_logits_op = logits_opt.minimize(loss_op, var_list=tr_vars)
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
 
-    import time
-
-    # global_steps = 0
-    # with tf.Session(config=config) as sess:
-    #
-    #     # saver.restore(sess, "/home/tdteach/data/checkpoint")
-    #
-    #     sess.run(init_op)
-    #     # print('Starting epoch %d / %d' % (ep + 1, options.num_epochs))
-    #     z = 0
-    #
-    #     while True:
-    #         z = z+1
-    #         print('iter %d: '%(z))
-    #         st_time = time.time()
-    #         try:
-    #             _, w = sess.run([train_logits_op, loss_op])
-    #             print(w)
-    #
-    #             # ims = np.asarray(ims)
-    #             # im = np.uint8((ims[0]+1)*127.5)
-    #             # Image.fromarray(im).show()
-    #             # cv2.waitKey()
-    #             # exit()
-    #
-    #         except tf.errors.OutOfRangeError:
-    #             break
-    #         print(time.time()-st_time)
-    #     global_steps += int(options.num_examples_per_epoch/options.batch_size)
-    #     saver.save(sess,'checkpoints/retrain_on', global_step=global_steps, write_meta_graph=False)
-
-
     rst_matrix = None
     rst_labels = None
     ans = 0
+
+    n_test_examples = int(5000)
+
+    test_var = None
+    for v in tf.global_variables():
+        if 'bn_conv1/mean' in v.name:
+            test_var = v
+            break
+    print(test_var.name)
+
+    update_all_op = tf.group(tf.get_collection(tf.GraphKeys.UPDATE_OPS))
+    init_op = tf.global_variables_initializer()
     with tf.Session(config=config) as sess:
         sess.run(init_op)
-        saver.restore(sess, "/home/tdteach/data/checkpoint/resnet101-60000")
+        saver.restore(sess, "/home/tdteach/data/checkpoint/resnet101-500000")
+
+
         # saver.restore(sess, "/home/tdteach/checkpoints/-10")
         # sess.run(init_op)
         # checkpoint_path = options.checkpoint_folder
         # saver.save(sess, checkpoint_path, global_step=10101)
-        for k in range(int(1000/options.batch_size)):
+        for k in range(int(n_test_examples/options.batch_size)):
+            print(k)
+
+            # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            # sess.run(update_ops)
+            # with tf.control_dependencies([variables_averages_op]):
             llgg, a, lbs = sess.run([logits, out_op, labels])
+            # sess.run(update_all_op)
+            # print('33333333333333333333333333333333333333333')
 
             tl = np.argmax(llgg, axis=1)
             for ii in range(options.batch_size):
@@ -310,7 +321,9 @@ def main():
                 rst_labels = np.concatenate((rst_labels,lbs))
 
 
-    print("acc: %.2f%%" % (ans/1000.0*100))
+    dataset.stop()
+
+    print("acc: %.2f%%" % (ans*1.0/n_test_examples*100))
 
     no = np.linalg.norm(rst_matrix, axis=1)
     aft = np.divide(rst_matrix.transpose(), no)
@@ -319,34 +332,36 @@ def main():
 
     print(np.shape(rst_labels))
 
-    z = rst_labels[0:1000]
-    z = z.repeat(1000).reshape(1000,1000).transpose()
-    for i in range(1000):
+    z = rst_labels[0:n_test_examples]
+    z = z.repeat(n_test_examples).reshape(n_test_examples,n_test_examples).transpose()
+    for i in range(n_test_examples):
         z[i] = z[i]-z[i,i]
     z = np.absolute(z)/10000.0
     z = 1 - np.ceil(z) + 0.01
     z = z.astype(np.int32)
 
-    # top-1
-    rt = 0
-    for i in range(1000):
-        if i == 0:
-            rt += z[i][np.argmax(coss[i][1:])]
-        elif i == 999:
-            rt += z[i][np.argmax(coss[i][:-1])]
-        else:
-            k1 = np.argmax(coss[i][0:i])
-            k2 = np.argmax(coss[i][i+1:])
-            if coss[i][k1] > coss[i][k2+i+1]:
-                rt += z[i][k1]
-            else:
-                rt += z[i][k2+i+1]
-
-    print("top1 : %.2f%%" % (rt/1000.0*100))
+    # # top-1
+    # rt = 0
+    # for i in range(1000):
+    #     if i == 0:
+    #         rt += z[i][np.argmax(coss[i][1:])]
+    #     elif i == 999:
+    #         rt += z[i][np.argmax(coss[i][:-1])]
+    #     else:
+    #         k1 = np.argmax(coss[i][0:i])
+    #         k2 = np.argmax(coss[i][i+1:])
+    #         if coss[i][k1] > coss[i][k2+i+1]:
+    #             rt += z[i][k1]
+    #         else:
+    #             rt += z[i][k2+i+1]
+    #
+    # print("top1 : %.2f%%" % (rt*1.0/n_test_examples*100))
 
     # ROC
+    print(z.shape)
+    print(coss.shape)
     from sklearn import metrics
-    fpr, tpr, thr =metrics.roc_curve(z.reshape(1,1000*1000).tolist()[0], coss.reshape(1,1000*1000).tolist()[0])
+    fpr, tpr, thr =metrics.roc_curve(z.reshape(1,n_test_examples*n_test_examples).tolist()[0], coss.reshape(1,n_test_examples*n_test_examples).tolist()[0])
 
 
     for i in range(len(fpr)):
